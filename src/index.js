06cdb31c1379c7cae06c4a9748e408ddbfac6e57
JerYOMat <jeremy.emata@gmail.com>
2019-03-19 16:43:19 -0400

    moved search components to own directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchComponent from './components/SearchComponent'
+import SearchComponent from './components/search/SearchComponent'
 import 'bootstrap/dist/css/bootstrap.css';
 const App = () => {
   return (
